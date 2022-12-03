70e63e24506017e793b23340dcdd22ceb969d149
thenns <samm77@gmail.com>
2022-12-02 19:23:27 -0700

    got server files running

--- server/controller.js
+++ server/controller.js
@@ -1,2 +1,13 @@
 const userServers = require('./server.json');
 
+module.exports = {
+    createServer: (req, res) => {
+        //create server here
+        res.status(200).send();
+
+    },
+    deleteServer: () => {
+        //delete server
+        res.status(200).send();
+    },
+}
--- server/index.js
+++ server/index.js
@@ -6,10 +6,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { //createServer
-        //deleteServera
+const { createServer,
+        deleteServer
       } = require('./controller')
 
+app.set('view engine', 'html');
+app.get('/', function (req, res) {
+    res.render('index', {});
+});
 app.get("/api/create_server", createServer);
 app.delete("/api/delete_server", deleteServer);
 app.listen(4000, () => console.log("server running on 4000"));
