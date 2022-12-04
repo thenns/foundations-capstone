bec41b05a9ae0b29c7ef96e8832ce874a1702f21
thenns <samm77@gmail.com>
2022-12-03 21:34:17 -0700

    getservers function displays current live servers

--- client/main.js
+++ client/main.js
@@ -2,7 +2,6 @@ const createServerButton = document.getElementById("createServerButton")
 const deleteServerButton = document.getElementById("deleteServerButton")
 const loadServersOnPageLoad = document.getElementById("serverStatus")
 
-
 const createServer = () => {
     axios.post("http://localhost:4000/api/create_server")
         .then(res => {
@@ -21,11 +20,18 @@ const getServers = () => {
     axios.get("http://localhost:4000/api/get_servers")
         .then(res => {
             const data = res.data;
-            console.log(data);
             let serverTable = '';
+            serverTable += "<div id='serversTable'><table><tr><th> server name </th>" +
+                           "<th> container name </th>" +
+                           "<th> port number </th>" +
+                           "<th> max # of players </th></tr>";
             data.forEach(function(item){
-                serverTable += 'server name: ' + item.serverName + '<br>';
+                serverTable += '<tr><td>'+ item.serverName + '</td>' +
+                               '<td>' + item.containerName + '</td>' +
+                               '<td>' + item.portNumber + '</td>' +
+                               '<td>' + item.maxPlayers + '</td></tr>';
             });
+            serverTable += '</table></div>'; 
             document.getElementById('serverStatus').innerHTML = serverTable;
         });
 };
@@ -35,6 +41,4 @@ console.log('delete server');
 console.log(deleteServer);
 console.log(deleteServerButton);
 deleteServerButton.addEventListener('click', deleteServer)
-//loadServersOnPageLoad.addEventListener('load', setInterval(getServers(), 5000);
 setInterval(getServers(), 5000);
-   // loadServersOnPageLoad.addEventListener('load', getServers)
--- server/controller.js
+++ server/controller.js
@@ -2,7 +2,6 @@ const userServers = require('./server.json');
 const { execSync } = require('child_process');
 const fs = require('fs');
 const fileName = './server.json';
-//const file = require(fileName);
 
 module.exports = {
     createServer: (req, res) => {
@@ -17,8 +16,9 @@ module.exports = {
                 maxPlayers,
                 onlineMode,
                 allowFlight } = req.body;
-        //console.log(req.body);
-        //better error handling checking
+        //For furture me: 
+        //check every variable to warn agains duplicate container names, server names and ports
+        //make error handling better
         execSync(`docker run -d -it --name ${containerName} -e MOTD="${serverDesc}" -e VERSION=${versionNumber} -p ${portNumber}:25565 -e EULA=TRUE -e DIFFICULTY=${difficulty} -e MODE=${gameMode} -e SERVER_NAME='${serverName}' -e MAX_PLAYERS=${maxPlayers} -e ONLINE_MODE=${onlineMode} -e ALLOW_FLIGHT=${allowFlight} itzg/minecraft-server`, 
             (error, stdout, stderr, ) => {
                 if (error) {
@@ -31,10 +31,8 @@ module.exports = {
                 }
                 console.log(`stdout: ${stdout}`);
         });
-        //console.log('ran docker command');
-        //let temp = fs.readFileSync(fileName);
+        //read json file and append new server to it
         let existingServers = JSON.parse(fs.readFileSync(fileName));
-        //console.log(existingServers);
         let newServer = {serverName:serverName, 
                          containerName:containerName,
                          portNumber:portNumber,
@@ -45,21 +43,22 @@ module.exports = {
         fs.writeFileSync(fileName, JSON.stringify(existingServers), function writeJSON(err) {
             if (err) return console.log(err);
             console.log(JSON.stringify(existingServers));
-        //    console.log('writing to ' + fileName);
         });
-
-        //res.status(200).send();
+        //redirect back to original page after completing the function
         res.writeHead(302, {
             'Location': 'http://localhost:4000/'
         });
         res.end();
-
     },
     deleteServer: (req, res) => {
-        //delete server
+        //For future me:
+        //create a delete server function 
+        //it will delete the server from json file and run a docker
+        //command to delete it from the server
         res.status(200).send();
     },
     getServers: (req, res) => {
+        //display a list of currently running servers
         let servers = JSON.parse(fs.readFileSync(fileName));
         console.log(servers);
         res.status(200).send(servers);
